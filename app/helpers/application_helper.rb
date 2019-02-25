395f52e455b9da7a86d4068813c7d16d2634ee8c
Peter Jang <peter.jang@veracross.com>
2019-02-25 14:55:05 -0500

    Authorize within scope

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def new_oauth_token_path
-    "#{ENV['server_base_url']}/vcsandbox/oauth/authorize?client_id=#{ENV['oauth_token']}&redirect_uri=#{ENV['oauth_redirect_uri']}&response_type=code"
+    "#{ENV['server_base_url']}/vcsandbox/oauth/authorize?client_id=#{ENV['oauth_token']}&redirect_uri=#{ENV['oauth_redirect_uri']}&scope=public sso&response_type=code"
   end
 end
